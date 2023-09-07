d8ab0e02e9d9e4cd93478daa09d7441137d49994
shantanupm171 <97331430+shantanupm171@users.noreply.github.com>
2023-09-07 18:38:09 +0530

    First commit FastApi_Todo

---
+++ main.py
@@ -0,0 +1,82 @@
+from fastapi import FastAPI, HTTPException, Depends
+from sqlalchemy import create_engine, Column, Integer, String, Boolean
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+from pydantic import BaseModel
+
+# SQLite database configuration
+DATABASE_URL = "sqlite:///./test.db"  # Use a local SQLite database file
+engine = create_engine(DATABASE_URL)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+Base = declarative_base()
+
+# SQLAlchemy Model
+class TodoItemDB(Base):
+    __tablename__ = "todo_items"
+    id = Column(Integer, primary_key=True, index=True)
+    title = Column(String, index=True)
+    description = Column(String)
+    done = Column(Boolean, default=False)
+
+Base.metadata.create_all(bind=engine)
+
+app = FastAPI()
+# Pydantic model for your to-do item
+
+class TodoItem(BaseModel):
+    title: str
+    description: str
+    done: bool = False
+
+# Dependency to get the database session
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+# CRUD operations
+@app.post("/todos/", response_model=TodoItem)
+def create_todo(todo: TodoItem, db = Depends(get_db)):
+    db_todo = TodoItemDB(**todo.dict())
+    db.add(db_todo)
+    db.commit()
+    db.refresh(db_todo)
+    return db_todo
+
+@app.get("/todos/{todo_id}", response_model=TodoItem)
+def read_todo(todo_id: int, db = Depends(get_db)):
+    db_todo = db.query(TodoItemDB).filter(TodoItemDB.id == todo_id).first()
+    if db_todo is None:
+        raise HTTPException(status_code=404, detail="Todo not found")
+    return db_todo
+
+@app.get("/todos/", response_model=list[TodoItem])
+def read_todos(skip: int = 0, limit: int = 10, db = Depends(get_db)):
+    todos = db.query(TodoItemDB).offset(skip).limit(limit).all()
+    return todos
+
+@app.put("/todos/{todo_id}", response_model=TodoItem)
+def update_todo(todo_id: int, todo: TodoItem, db = Depends(get_db)):
+    db_todo = db.query(TodoItemDB).filter(TodoItemDB.id == todo_id).first()
+    if db_todo is None:
+        raise HTTPException(status_code=404, detail="Todo not found")
+    for key, value in todo.dict().items():
+        setattr(db_todo, key, value)
+    db.commit()
+    db.refresh(db_todo)
+    return db_todo
+
+@app.delete("/todos/{todo_id}", response_model=TodoItem)
+def delete_todo(todo_id: int, db = Depends(get_db)):
+    db_todo = db.query(TodoItemDB).filter(TodoItemDB.id == todo_id).first()
+    if db_todo is None:
+        raise HTTPException(status_code=404, detail="Todo not found")
+    db.delete(db_todo)
+    db.commit()
+    return db_todo
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, port=8000)
